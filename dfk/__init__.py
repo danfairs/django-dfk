5617d0c62e53a2f6d814375248701c546429d55c
Dan Fairs <dan@fezconsulting.com>
2010-11-08 20:11:20 +0000

    Mechanism for overriding rel's kwargs

--- dfk/__init__.py
+++ dfk/__init__.py
@@ -1,15 +1,16 @@
-def repoint(from_model, rel_name, to_model):
+def repoint(from_model, rel_name, to_model, **kwargs):
     options = getattr(to_model, '_meta', None)
     if options:
         assert not options.abstract, u'Target model may not be abstract'
     field = from_model._meta.get_field(rel_name)
     rel = field.rel
     args = (to_model, rel.field_name)
-    kwargs = {
+    new_kwargs = {
         'related_name': rel.related_name,
         'limit_choices_to': rel.limit_choices_to,
         'lookup_overrides': rel.lookup_overrides,
         'parent_link': rel.parent_link
     }
-    new_rel = rel.__class__(*args, **kwargs)
+    new_kwargs.update(kwargs)
+    new_rel = rel.__class__(*args, **new_kwargs)
     field.rel = new_rel
