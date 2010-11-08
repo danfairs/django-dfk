146d01a39a204c5ed90f20b71717c9831c68686f
Dan Fairs <dan@fezconsulting.com>
2010-11-08 20:07:51 +0000

    Initial version, tests pass

--- dfk/__init__.py
+++ dfk/__init__.py
@@ -1,2 +1,15 @@
-def repoint(from_model, fk_name, to_model):
-    pass
+def repoint(from_model, rel_name, to_model):
+    options = getattr(to_model, '_meta', None)
+    if options:
+        assert not options.abstract, u'Target model may not be abstract'
+    field = from_model._meta.get_field(rel_name)
+    rel = field.rel
+    args = (to_model, rel.field_name)
+    kwargs = {
+        'related_name': rel.related_name,
+        'limit_choices_to': rel.limit_choices_to,
+        'lookup_overrides': rel.lookup_overrides,
+        'parent_link': rel.parent_link
+    }
+    new_rel = rel.__class__(*args, **kwargs)
+    field.rel = new_rel
